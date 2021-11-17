f3fa2d14b1de13ca93909e42c864a145b136acda
Pooja Kamal <poojakamal@Poojas-MacBook-Air.local>
2021-11-17 09:19:39 -0600

    done

---
+++ src/HomePage/HomePage.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { userService } from '../_services';
+
+class HomePage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            user: {},
+            users: []
+        };
+    }
+
+    render() {
+        const { user, users } = this.state;
+        return (
+            <div className="col-md-6 col-md-offset-3">
+                <h1>Hi {user.firstName}!</h1>
+                <p>You're logged in!!</p>
+                <p>
+                    <Link to="/login">Logout</Link>
+                </p>
+            </div>
+        );
+    }
+}
+
+export { HomePage };
